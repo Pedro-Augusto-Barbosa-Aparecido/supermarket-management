6b8acb27021b6a0b27b35ff8e92e043f8866d683
Pedro Augusto <pedro007augustobarbosa@gmail.com>
2022-02-27 21:31:50 -0300

    configuring react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import "./globals/reset.css";
+
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Routers from './routes';
+
 function App() {
   return (
-    <div className="App">
+    <div style={{height: "100vh"}}>
+      <Header />
+      <Menu />
+      <Routers />
     </div>
   );
 }
